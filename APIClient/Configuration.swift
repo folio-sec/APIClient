4edc5e8196ea16bf8c51159f644b0335f1266c37
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2019-03-12 05:28:27 +0900

    Code format

--- APIClient/Configuration.swift
+++ APIClient/Configuration.swift
@@ -9,7 +9,11 @@ public struct Configuration {
 
     public let queue: DispatchQueue
 
-    public init(dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .iso8601, dataDecodingStrategy: JSONDecoder.DataDecodingStrategy = .base64, timeoutIntervalForRequest: TimeInterval = 60, timeoutIntervalForResource: TimeInterval = 604800, queue: DispatchQueue = DispatchQueue.main) {
+    public init(dateDecodingStrategy: JSONDecoder.DateDecodingStrategy = .iso8601,
+                dataDecodingStrategy: JSONDecoder.DataDecodingStrategy = .base64,
+                timeoutIntervalForRequest: TimeInterval = 60,
+                timeoutIntervalForResource: TimeInterval = 604800,
+                queue: DispatchQueue = DispatchQueue.main) {
         self.dateDecodingStrategy = .iso8601
         self.dataDecodingStrategy = .base64
         self.timeoutIntervalForRequest = timeoutIntervalForRequest
